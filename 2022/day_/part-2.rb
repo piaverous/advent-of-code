e2b725a66e9d60462bb191f08bb817e156526d21
Pierre Averous <piaverous@gmail.com>
2022-12-09 22:07:30 +0100

    feat(2022/framework): commit daily template

---
+++ 2022/day_/part-2.rb
@@ -0,0 +1,38 @@
+#!/usr/bin/ruby -w
+require 'benchmark'
+BEGIN {
+    $args = {}
+
+    ARGV.each do |arg|
+        match = /--(?<key>.*)/.match(arg)
+        $args[match[:key]] = true # e.g. args['bench'] = true
+    end
+}
+
+
+class AdventSolver
+    def initialize(title)
+        @title = title
+        puts "Starting #@title problem solving..."
+
+        @lines = File.readlines("input.txt", chomp: true)
+        puts "Read input file with #{@lines.length} lines"
+    end
+
+    def solve 
+        # TODO: Implement
+    end
+end
+
+
+part = __FILE__.split(".")[0][-1]
+day = File.basename(__dir__)[-1]
+
+$solver = AdventSolver.new("AOC2022 Day #{day} Part #{part}")
+if $args["bench"]
+    Benchmark.bm do |x| 
+        x.report("solving") { $solver.solve }
+    end
+else
+    puts $solver.solve
+end
